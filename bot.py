5d79ef4ee3856d1a9833b7045f85ae60cb78be60
slayerch1 <cesarjmorais339@gmail.com>
2023-11-06 12:10:56 -0300

    corrigindo bugs no bot

--- bot.py
+++ bot.py
@@ -11,13 +11,13 @@ driver = editacodigo_whats.carregar_pagina_whatsapp('zap/sessao','https://web.wh
 
 ########### VARIAVEIS #######
 
-usuario = 'testebot@gmail.com'
+usuario = 'julio@gmail.com'
 
-pagina = 'http://localhost/bot/api/recebe.php?'
+pagina = 'http://localhost/paniel-adm-robo/api/recebe.php?'
 
-servidor_enviar = 'http://localhost/bot/api/enviar.php?'
+servidor_enviar = 'http://localhost/paniel-adm-robo/api/enviar.php?'
 
-servidor_confirmar = 'http://localhost/bot/api/confirma.php?'
+servidor_confirmar = 'http://localhost/paniel-adm-robo/api/confirma.php?'
 #############################################################################
 
 while len(driver.find_elements(By.ID,'side')) < 1 : 
@@ -26,14 +26,17 @@ while len(driver.find_elements(By.ID,'side')) < 1 :
 while True:
     try:
         notificacao =  editacodigo_whats.abrir_notificacao(driver,bolinha_notificacao,pega_contato,contato_cliente,msg_cliente,usuario,pagina)
-        if notificacao :
-                telefone_final = editacodigo_whats.pega_contato(driver,contato_cliente)
-                final = editacodigo_whats.envia_as_msg_para_servidor(driver,msg_cliente,telefone_final,usuario,pagina)
-                envia = editacodigo_whats.enviar_msg_do_servidor(driver, servidor_enviar,usuario,caixa_de_pesquisa,caixa_msg,servidor_confirmar)
-                print(envia)
-    except Exception as e:
-         print(f"Ocorreu uma exceção: {e}")
-         time.sleep(5)
-       
+        time.sleep(1)
+        telefone_final = editacodigo_whats.pega_contato(driver,contato_cliente)
+        time.sleep(1)
+        final = editacodigo_whats.envia_as_msg_para_servidor(driver,msg_cliente,telefone_final,usuario,pagina)
+    except:
+        try:
+            envia = editacodigo_whats.enviar_msg_do_servidor(driver, servidor_enviar,usuario,caixa_de_pesquisa,caixa_msg,servidor_confirmar)
+            print(envia)
+        except:
+            print('aguarde')    
         
        
+        
+      
